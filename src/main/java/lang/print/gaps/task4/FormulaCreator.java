a8acd853007a5037b4dc9c35dac531ac4ee8c5ea
d.anisimov <d.anisimov@ds-manager03.szspspb.local>
2023-04-10 19:12:24 +0300

    Stage 0 Module 3 Task 4 done.

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -1,6 +1,11 @@
 package lang.print.gaps.task4;
 
+
 public class FormulaCreator {
     public static void main(String[] args) {
+    	int a, b, c;
+    	a = 1;
+    	b = 2;
+    	c = ((int) (9 * Math.pow(a, 2)) + 5 * b + 2 + a - 7) * ((a + b - 4 * a * b)/ 2);
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,11 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+    	
+    	char aCharInInt = 97;
+    	char cCharInInt = 99;
+    	char hCharInInt = 104;
+    	char rCharInInt = 114;
+    	System.out.println(cCharInInt + " " + hCharInInt + " " + aCharInInt + " " + rCharInInt);
     }
 }
