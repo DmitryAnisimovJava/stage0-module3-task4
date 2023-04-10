61bc7c613f7efc59f73535a77c9bbccfa49b9925
d.anisimov <d.anisimov@ds-manager03.szspspb.local>
2023-04-10 19:20:59 +0300

    Stage 0 Module 3 Task 4

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -3,9 +3,9 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
-    	int a, b, c;
+    	double a, b, c;
     	a = 1;
     	b = 2;
-    	c = ((int) (9 * Math.pow(a, 2)) + 5 * b + 2 + a - 7) * ((a + b - 4 * a * b)/ 2);
+    	c = (9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
     }
 }
